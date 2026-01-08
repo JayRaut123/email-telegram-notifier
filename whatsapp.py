04e8367133510ecccdcc2d9863b0802bce18743c
Jay123 <rautjay12345@student.sfit.ac.in>
2026-01-08 23:44:15 +0530

    Changed

--- whatsapp.py
+++ whatsapp.py
@@ -1,19 +1,20 @@
-from twilio.rest import Client
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+#from twilio.rest import Client
+#import os
+#from dotenv import load_dotenv
 
-ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-FROM_WHATSAPP = os.getenv("TWILIO_FROM_WHATSAPP")
-TO_WHATSAPP = os.getenv("TWILIO_TO_WHATSAPP")
+#load_dotenv()
 
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+#ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+#AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+#FROM_WHATSAPP = os.getenv("TWILIO_FROM_WHATSAPP")
+#TO_WHATSAPP = os.getenv("TWILIO_TO_WHATSAPP")
 
-def send_whatsapp(msg):
-    client.messages.create(
-        body=msg,
-        from_=FROM_WHATSAPP,
-        to=TO_WHATSAPP
-    )
+#client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+#def send_whatsapp(msg):
+#    client.messages.create(
+#        body=msg,
+#        from_=FROM_WHATSAPP,
+#        to=TO_WHATSAPP
+#    )
